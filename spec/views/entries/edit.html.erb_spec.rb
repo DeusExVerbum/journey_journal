36ea13f6691858cfa9369053df5466c7d93746be
DeusExVerbum <mymorningpromenade@gmail.com>
2014-09-19 16:36:37 -0700

    rails g scaffold Entry title:string body:text
    Created scaffold for Entry

---
+++ spec/views/entries/edit.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "entries/edit", :type => :view do
+  before(:each) do
+    @entry = assign(:entry, Entry.create!(
+      :title => "MyString",
+      :body => "MyText"
+    ))
+  end
+
+  it "renders the edit entry form" do
+    render
+
+    assert_select "form[action=?][method=?]", entry_path(@entry), "post" do
+
+      assert_select "input#entry_title[name=?]", "entry[title]"
+
+      assert_select "textarea#entry_body[name=?]", "entry[body]"
+    end
+  end
+end
